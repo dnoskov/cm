e7efb2ebff4d463a4e6dd9152859217767d47456
dnoskov <nda85@ya.ru>
2016-04-19 00:53:37 +0400

    fixed relaxation method - it works now! ypeeeeee!

--- src/main/scala/lab02/Relax.scala
+++ src/main/scala/lab02/Relax.scala
@@ -7,54 +7,28 @@ case class Relax(L: Array[Array[Double]], r: Array[Double],
   omega: Double,
   x0: Array[Double] = Array.empty[Double]) extends Iterative(L,r,eps,x0) {
 
+  val debug = false
 
-  val xtmp = x0.clone
+  def iterate: Unit = {
+    (0 until dim).map( i => x0(i) = x1(i))
+    for (i <- 0 until dim) {
+      var s: Double = 0
+      for (j <- 0 until i) s += A(i)(j) * x1(j)
+      for (j <- i+1 until dim) s += A(i)(j) * x0(j)
 
-  println("x0 = " + rowRepr(x0))
-  // Находим первое приближение
-  for (i <- 0 until dim) {
-    println(f"     x1($i) = ")
-    println(f"          $omega/A($i)($i) = ${omega/A(i)(i)} * ")
-    println(f"          ( f($i) = " + f(i) + " - ")
-    println(f"          (0 until $i).map( j => A($i)(j) * x1(j)).sum = " + (0 until i).map( j => A(i)(j) * x1(j)).sum + " - ")
-    println(f"          (${i+1} until $dim).map( j => A(i)(j) * x0(j)).sum = " + (i+1 until dim).map( j => A(i)(j) * x0(j)).sum + ") + ")
-    println(f"          (1 - $omega) * x0($i) = " + (1 - omega) * x0(i))
+      x1(i) = (omega/A(i)(i)) * (f(i) - s) + (1 - omega) * x0(i)
 
-    x1(i) = (omega/A(i)(i)) * (f(i)
-      - (0 until i).map( j => A(i)(j) * x1(j)).sum
-      - (i+1 until dim).map( j => A(i)(j) * x0(j)).sum)
-      + (1 - omega) * x0(i)
+      iterations += 1
+
+    }
+
   }
-  println("x1 = " + rowRepr(x1))
-  println
 
 
   // Начинаем итерации
-  iterations += 1
-  while(!endCondition) {
-    (0 until dim).map( i => x0(i) = x1(i))
-
-    println("x0 = " + rowRepr(x0))
-
-    for (i <- 0 until dim) {
-
-      println(f"     x1($i) = ")
-      println(f"          $omega/A($i)($i) = ${omega/A(i)(i)} * ")
-      println(f"          ( f($i) = " + f(i) + " - ")
-      println(f"          (0 until $i).map( j => A($i)(j) * x1(j)).sum = " + (0 until i).map( j => A(i)(j) * x1(j)).sum + " - ")
-      println(f"          (${i+1} until $dim).map( j => A(i)(j) * x0(j)).sum = " + (i+1 until dim).map( j => A(i)(j) * x0(j)).sum + ") + ")
-      println(f"          (1 - $omega) * x0($i) = " + (1 - omega) * x0(i))
-
-      x1(i) = (omega/A(i)(i)) * (f(i)
-        - (0 until i).map( j => A(i)(j) * x1(j)).sum
-        - (i+1 until dim).map( j => A(i)(j) * x0(j)).sum)
-        + (1 - omega) * x0(i)
-    }
-    println("x1 = " + rowRepr(x1))
-    println
-
-    iterations += 1
-  }
+  do {
+    iterate
+  }while(!endCondition)
 
   for (i <- 0 until dim)
     x(i) = x0(i)
